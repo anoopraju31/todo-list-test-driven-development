7a05b858a9f4766881b97f8b442aeab6ebe6b102
anoopraju31 <anoopraju31@gmail.com>
2023-11-15 13:26:05 +0530

    Todo - refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import Todo from './components/Todo'
+
 function App() {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Todo items={[]} />
 		</div>
 	)
 }
